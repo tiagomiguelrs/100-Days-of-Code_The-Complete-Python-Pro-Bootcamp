46f0656e31d0165f96736f7fe09fe945e951ca49
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-07 09:40:36 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,26 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+
+# Using int() floors the number
+print(int(bmi))
+
+# Using round() rounds the number to the nearest integer
+print(round(bmi))
+
+# But round() takes to positional arguments
+print(round(bmi, 2))
+
+# Assignment will take the variable value (integer or float) and perform the
+# operation before the equal sign like score = score + value
+score = 10
+
+score += 2
+print(score)
+
+score -= 5
+print(score)
+
+# f-strings
+is_winning = True
+print(f"The score is {score} and your bmi is {bmi}, but are you winning?\n")
+print(f"{is_winning==True}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -4,3 +4,7 @@ tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
 
+pay = (bill / people) * (1 + tip / 100)
+print(f"Each person should pay {round(pay, 2):.2f}$")
+# If I only use round(pay, 2) I will only get 33.6 because the next number is 0
+# and it is not shown
