665c18730fc72cc39e6311bfc91d86553dfb1034
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-07 08:31:54 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,9 @@
-# Write your code below this line 👇
+print("Hello nameless friend who belongs in a nameless band!"
+      "\nTo find the best name for your band please provide:"
+      "\n\n")
+
+city_name = input("The name of the city you grew up in bellow:\n")
+
+pet_name = input("Also, the name of a pet of yours! (Can be a name of a sibling hehe):\n")
+
+print("\nYour incredibly innovative band name is.... *drumroll*...\n\n" + city_name + " " + pet_name + "!")
