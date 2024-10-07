a6e584591d6ae19f884826392b5f0e51b50d6398
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-07 08:44:56 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,18 @@
-len("Hello")
+# Subscripting
+print("Hello"[0])
+
+# String
+print("123" + "456")
+
+# Integer = Whole number
+print(123 + 456)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
