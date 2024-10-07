2c7a56dcb6e05bda914f1428a4a7002ac69f2758
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-07 08:59:23 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,17 @@
-len(12345)
+print(len("12345"))
+
+# Type of an object
+# String
+print(type("Hello"))
+
+# Integer
+print(type(123))
+
+# Float
+print(type(3.14159))
+
+# Boolean
+print(type(True))
+
+# print("Number of letters in your name: " + len(input("Enter your name")))
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
