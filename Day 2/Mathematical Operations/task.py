8599d63c28be0f1150bd835b7c90426863577850
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-07 09:15:33 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,20 @@
 print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(7 / 3)
+# Even though 6 devides cleanly by 3, it will become a floating point number
+# This is called implicit type casting
+print(7 // 3)
+# Using // there will be no type casting and the user will get the
+# integer number only
+print(2 ** 3)
+
+# PEMDAS(left to right)
+# ()
+# **
+# * OR /
+# + OR -
+
+# print(3 * (3 + 3) / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
