9d351d8f046520567df292dfdd79e6ac7f401fa3
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-08 08:06:01 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -3,5 +3,12 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("How old are you? "))
+    if age <= 12:
+        print("Pay 5 $ please.")
+    elif age <= 18:
+        print("Pay 7 $ please.")
+    else:
+        print("Pay 12 $ please.")
 else:
     print("Sorry you have to grow taller before you can ride.")
