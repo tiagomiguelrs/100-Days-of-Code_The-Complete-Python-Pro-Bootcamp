8362690ad336012d8c09c00b1979c85afd444953
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-08 08:19:49 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+bill = 0
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        print("Children tickets are $5.")
+        bill += 5
     elif age <= 18:
-        print("Please pay $7.")
+        print("Teens tickets are $7.")
+        bill += 7
     else:
-        print("Please pay $12.")
+        print("Adult tickets are $12.")
+        bill += 12
+
+    photo = input("Do you want photo?: ")
+    if photo == "yes":
+        print("Photo will be extra 3 $")
+        bill += 3
+
+    print(f"The bill is {bill}")
+
 else:
     print("Sorry you have to grow taller before you can ride.")
