445361bcfc3d35fcb94608e2ab2c86c3193f3281
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-08 11:11:25 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,15 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+n = random.randint(0, len(friends)-1)
+
+pick = friends[n]
+
+print(f"Chosen friend is {pick}")
+
+# OR
+
+pick = random.choice(friends)
+
+print(f"Chosen friend is {pick}")
