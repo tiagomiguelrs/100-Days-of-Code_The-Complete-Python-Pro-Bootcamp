d9b80f3b71694dd45faa0242e7c0633cb8a21c41
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-08 08:55:59 +0100

    Raw version of the first 15 lessons of 101 Days of Code Bootcamp by Angela Yu

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -4,6 +4,7 @@ pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
 bill = 0
+wrong_flag = False
 if size == "S":
     bill = 15
     if pepperoni == "Y":
@@ -12,12 +13,15 @@ elif size == "M":
     bill = 20
     if pepperoni == "Y":
         bill += 3
-else:
+elif size == "L":
     bill = 25
     if pepperoni == "Y":
         bill += 3
+else:
+    print("You printed the wrong inputs.")
+    wrong_flag = True
 
-if extra_cheese == "Y":
+if not wrong_flag and extra_cheese == "Y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")
