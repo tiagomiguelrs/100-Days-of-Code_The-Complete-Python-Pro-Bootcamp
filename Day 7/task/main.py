8b0b7ca5f7ac0628ae31a5f3c2ff2eed03acacdb
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-10 08:58:07 +0100

    Update to bootcamp completion up to day 5

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,40 @@
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+# placeholder = "".join(["_" for letter in chosen_word])
+# OR
+placeholder = ""
+for letter in chosen_word:
+    placeholder += "_"
+
+guess = input("Guess a letter: ").lower()[0]
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+# display = []
+# for i in range(len(chosen_word)):
+#     if guess == chosen_word[i]:
+#         display.append(guess)
+#         print("Right")
+#     else:
+#         display.append("_")
+#         print("Wrong")
+#
+# print("".join([letter for letter in display]))
+
+# OR
+
+display = ""
+for i in range(len(chosen_word)):
+    if guess == chosen_word[i]:
+        display += guess
+        print("Right")
+    else:
+        display += "_"
+        print("Wrong")
+
+print(display)
