4a682ba8a81b86e51b13087cc3694f35740a81c4
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-11 19:14:06 +0100

    Update to bootcamp completion up to day 8

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -10,13 +10,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
 
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
     for letter in original_text:
         if letter not in alphabet:
             output_text += letter
         else:
-            if encode_or_decode == "decode":
-                shift_amount *= -1
-
             shifted_position = alphabet.index(letter) + shift_amount
             shifted_position %= len(alphabet)
             output_text += alphabet[shifted_position]
