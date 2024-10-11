fb7c391f397f35a6f6441b18c47a45a86e78dbeb
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-11 18:17:52 +0100

    Update to bootcamp completion up to day 8

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,17 +1,37 @@
+from math import ceil
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+def encrypt(original_text, shift_amount):
+
+    # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+    #  by the shift amount and print the encrypted text.
+    shifted_text = ""
+    for l in original_text.lower():
+        if l != " ":
+            original_index = alphabet.index(l)
+            shifted_index = original_index + shift_amount
+            # If new letter index is greater than list length, restart index
+            if shifted_index >= len(alphabet):
+                rotations = ceil(shifted_index//len(alphabet))
+                max_length = len(alphabet)
+                shifted_index -= int(max_length * rotations)
+            shifted_text += alphabet[shifted_index]
+        else:
+            shifted_text += " "
+
+    print(shifted_text)
 
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
 
+encrypt(text, shift)
--- Day 8/Positional vs Keyword Arguments/Day8 Love Calculator Exercise.py
+++ Day 8/Positional vs Keyword Arguments/Day8 Love Calculator Exercise.py
@@ -31,4 +31,28 @@ def calculate_love_score(name1, name2):
     print(f"Love Score = {total1}{total2}")
 
 
-calculate_love_score("Tiago", "Daniela")
+calculate_love_score("Tiago Santos", "Daniela Monteiro")
+
+# OR
+
+def calculate_love_score(name1, name2):
+    combined_names = name1 + name2
+    lower_names = combined_names.lower()
+
+    t = lower_names.count("t")
+    r = lower_names.count("r")
+    u = lower_names.count("u")
+    e = lower_names.count("e")
+    first_digit = t + r + u + e
+
+    l = lower_names.count("l")
+    o = lower_names.count("o")
+    v = lower_names.count("v")
+    e = lower_names.count("e")
+    second_digit = l + o + v + e
+
+    score = int(str(first_digit) + str(second_digit))
+    print(score)
+
+
+calculate_love_score("Tiago Santos", "Daniela Monteiro")
