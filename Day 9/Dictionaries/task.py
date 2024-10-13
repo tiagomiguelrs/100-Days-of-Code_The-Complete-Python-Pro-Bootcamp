0fc52e13e7036b20dd0ae6fbe34080207fceb14e
tiagomiguelrs <tiagomiguelrs@gmail.com>
2024-10-13 20:53:36 +0100

    Update to bootcamp completion up to day 8

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,23 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary["Loop"])
+
+# Wipe an existing dictionary
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Editing a dictionary entry
+# programming_dictionary["Bug"] = "A moth in your computer."
+# print(programming_dictionary["Bug"]
+
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
